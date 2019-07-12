31b9d1ff14cbaea2bcef92b5c5756fc8fdeb644e
Mathieu LESNIAK <mathieu@lesniak.fr>
2019-07-12 22:48:02 +0200

    to km/h

--- src/Unit/Speed.php
+++ src/Unit/Speed.php
@@ -4,25 +4,22 @@ namespace METAR\Unit;
 
 class Speed
 {
-
     /**
      * @var meters per second
      */
     protected $mps;
 
-
     public function __construct($value, $unit = 'kt')
     {
         if ($unit == 'm/s') {
-            $this->value = (float)$value;
+            $this->value = (float) $value;
         } elseif ($unit == 'km/s') {
-            $this->value = ((float)$value)*1000;
+            $this->value = ((float) $value) * 1000;
         } elseif ($unit == 'kt') {
-            $this->value = ((float)$value)*0.51444444444;
+            $this->value = ((float) $value) * 0.51444444444;
         } else {
             throw new \Exception("Unit must be one of m/s,km/s,kt");
         }
-
     }
 
     public function toUnit($unit)
@@ -31,12 +28,15 @@ class Speed
             return $this->value;
         }
         if ($unit == 'km/s') {
-            return $this->value/1000;
+            return $this->value / 1000;
+        }
+        if ($unit == 'km/h') {
+            return round($this->value * 3.6);
         }
         if ($unit == 'kt') {
-            return round($this->value*1.9438444924574);
+            return round($this->value * 1.9438444924574);
         }
 
         throw new \Exception("Unsupported unit");
     }
-} 
+}
